dd1b4d6a8e8cc5a9a87430abfa8378087f3c3e08
xhh <303158131@qq.com>
2020-06-20 17:23:14 +0800

    修改认证服务器父工程的BUG

--- febs-auth/src/test/java/com/javaboy/febs/auth/FebsAuthApplicationTests.java
+++ febs-auth/src/test/java/com/javaboy/febs/auth/FebsAuthApplicationTests.java
@@ -1,13 +1,13 @@
 package com.javaboy.febs.auth;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FebsAuthApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
