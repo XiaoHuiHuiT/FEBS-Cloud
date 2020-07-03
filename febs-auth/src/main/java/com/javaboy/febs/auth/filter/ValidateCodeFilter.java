01fedc15da1ea13a4acb6ef1b68d5e4b4432d21f
xhh <303158131@qq.com>
2020-07-03 16:53:05 +0800

    Swagger2OAth2认证整合完善

--- febs-auth/src/main/java/com/javaboy/febs/auth/filter/ValidateCodeFilter.java
+++ febs-auth/src/main/java/com/javaboy/febs/auth/filter/ValidateCodeFilter.java
@@ -18,6 +18,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @Component
@@ -25,8 +27,8 @@ import java.io.IOException;
  * @author jonath_yh
  */
 /* ValidateCodeFilter继承Spring Boot提供的OncePerRequestFilter
-*  该过滤器实现了javax.servlet.filter接口，顾名思义，它可以确保我们的逻辑只被执行一次
-* */
+ *  该过滤器实现了javax.servlet.filter接口，顾名思义，它可以确保我们的逻辑只被执行一次
+ * */
 public class ValidateCodeFilter extends OncePerRequestFilter {
 
     private final ValidateCodeService validateCodeService;
@@ -37,6 +39,7 @@ public class ValidateCodeFilter extends OncePerRequestFilter {
 
     /**
      * 拦截的请求URI为/oauth/token，请求方法为POST并且请求参数grant_type为password的时候（对应密码模式获取令牌请求），需要进行验证码校验。验证码校验调用的是之前定义的ValidateCodeService的check方法。当验证码调用通过时调用filterChain.doFilter(httpServletRequest, httpServletResponse)，让过滤器链继续往下执行，校验不通过时直接返回错误响应
+     *
      * @param httpServletRequest
      * @param httpServletResponse
      * @param filterChain
@@ -45,9 +48,15 @@ public class ValidateCodeFilter extends OncePerRequestFilter {
      */
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String header = httpServletRequest.getHeader("Authorization");
+        // getClientId这个方法用于从请求头部获取ClientId信息，这段代码是从Spring Cloud OAuth2源码中拷贝过来的，所以看不懂没关系，只要知道它的作用就行了
+        String clientId = getClientId(header, httpServletRequest);
+
         RequestMatcher matcher = new AntPathRequestMatcher("/oauth/token", HttpMethod.POST.toString());
+        // 获取了ClientId后，我们判断ClientId是否为swagger，是的话无需进行图形验证码校验
         if (matcher.matches(httpServletRequest)
-                && StringUtils.equalsIgnoreCase(httpServletRequest.getParameter("grant_type"), "password")) {
+                && StringUtils.equalsIgnoreCase(httpServletRequest.getParameter("grant_type"), "password")
+                && !StringUtils.equalsAnyIgnoreCase(clientId, "swagger")) {
             try {
                 validateCode(httpServletRequest);
                 filterChain.doFilter(httpServletRequest, httpServletResponse);
@@ -66,5 +75,22 @@ public class ValidateCodeFilter extends OncePerRequestFilter {
         String code = httpServletRequest.getParameter("code");
         String key = httpServletRequest.getParameter("key");
         validateCodeService.check(key, code);
+    }
+
+    private String getClientId(String header, HttpServletRequest request) {
+        String clientId = "";
+        try {
+            byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
+            byte[] decoded;
+            decoded = Base64.getDecoder().decode(base64Token);
+
+            String token = new String(decoded, StandardCharsets.UTF_8);
+            int delim = token.indexOf(":");
+            if (delim != -1) {
+                clientId = new String[]{token.substring(0, delim), token.substring(delim + 1)}[0];
+            }
+        } catch (Exception ignore) {
+        }
+        return clientId;
     }
 }
--- febs-server/febs-server-system/src/main/java/com/javaboy/febs/server/system/configure/FebsWebConfigure.java
+++ febs-server/febs-server-system/src/main/java/com/javaboy/febs/server/system/configure/FebsWebConfigure.java
@@ -8,15 +8,17 @@ import com.javaboy.febs.server.system.properties.FebsSwaggerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -43,8 +45,9 @@ public class FebsWebConfigure {
     /**
      * swaggerApi方法的apis(RequestHandlerSelectors.basePackage("com.javaboy.febs.server.system.controller"))
      * 表示将com.javaboy.febs.server.system.controller路径下的所有Controller都添加进去
-     *
+     * <p>
      * paths(PathSelectors.any())表示Controller里的所有方法都纳入
+     *
      * @return Docket
      */
     @Bean
@@ -55,11 +58,14 @@ public class FebsWebConfigure {
                 .apis(RequestHandlerSelectors.basePackage(swagger.getBasePackage()))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo(swagger));
+                .apiInfo(apiInfo(swagger))
+                .securitySchemes(Collections.singletonList(securityScheme(swagger)))
+                .securityContexts(Collections.singletonList(securityContext(swagger)));
     }
 
     /**
      * apiInfo用于定义一些API页面信息，比如作者名称，邮箱，网站链接，开源协议等等
+     *
      * @return ApiInfo
      */
     private ApiInfo apiInfo(FebsSwaggerProperties swagger) {
@@ -70,5 +76,47 @@ public class FebsWebConfigure {
                 null,
                 new Contact(swagger.getAuthor(), swagger.getUrl(), swagger.getEmail()),
                 swagger.getLicense(), swagger.getLicenseUrl(), Collections.emptyList());
+    }
+
+    /**
+     * securitySchemes：用于配置安全策略，比如配置认证模型，scope等内容
+     * securityScheme方法中，我们通过OAuthBuilder对象构建了安全策略，
+     * 主要配置了认证类型为ResourceOwnerPasswordCredentialsGrant（即密码模式），
+     * 认证地址为http://localhost:8301/auth/oauth/token（即通过网关转发到认证服务器），
+     * scope为test，和febs-auth模块里定义的一致。这个安全策略我们将其命名为febs_oauth_swagger
+     *
+     * @param swagger
+     * @return SecurityScheme
+     */
+    private SecurityScheme securityScheme(FebsSwaggerProperties swagger) {
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant(swagger.getGrantUrl());
+
+        return new OAuthBuilder()
+                .name(swagger.getName())
+                .grantTypes(Collections.singletonList(grantType))
+                .scopes(Arrays.asList(scopes(swagger)))
+                .build();
+    }
+
+
+    /**
+     * securityContexts：用于配置安全上下文，只有配置了安全上下文的接口才能使用令牌获取资源
+     * securityContext方法中，我们通过febs_oauth_swagger名称关联了上面定义的安全策略，
+     * 并且通过forPaths(PathSelectors.any())设置所有API接口都用这个安全上下文
+     *
+     * @param swagger
+     * @return SecurityContext
+     */
+    private SecurityContext securityContext(FebsSwaggerProperties swagger) {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference(swagger.getName(), scopes(swagger))))
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private AuthorizationScope[] scopes(FebsSwaggerProperties swagger) {
+        return new AuthorizationScope[]{
+                new AuthorizationScope(swagger.getScope(), "")
+        };
     }
 }
