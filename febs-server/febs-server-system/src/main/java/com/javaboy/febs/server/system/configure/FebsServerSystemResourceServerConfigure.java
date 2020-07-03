ca5d9675c223314373d3c901de0bc2b8fa6aac5a
xhh <303158131@qq.com>
2020-07-03 14:59:54 +0800

    接入Swagger2

--- febs-server/febs-server-system/src/main/java/com/javaboy/febs/server/system/configure/FebsServerSystemResourceServerConfigure.java
+++ febs-server/febs-server-system/src/main/java/com/javaboy/febs/server/system/configure/FebsServerSystemResourceServerConfigure.java
@@ -2,6 +2,8 @@ package com.javaboy.febs.server.system.configure;
 
 import com.javaboy.febs.common.handler.FebsAccessDeniedHandler;
 import com.javaboy.febs.common.handler.FebsAuthExceptionEntryPoint;
+import com.javaboy.febs.server.system.properties.FebsServerSystemProperties;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +22,9 @@ public class FebsServerSystemResourceServerConfigure extends ResourceServerConfi
     @Autowired
     private FebsAuthExceptionEntryPoint exceptionEntryPoint;
 
+    @Autowired
+    private FebsServerSystemProperties properties;
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.authenticationEntryPoint(exceptionEntryPoint)
@@ -28,10 +33,14 @@ public class FebsServerSystemResourceServerConfigure extends ResourceServerConfi
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        String[] anonUrls = StringUtils.splitByWholeSeparatorPreserveAllTokens(properties.getAnonUrl(), ",");
+
         http.csrf().disable()
                 .requestMatchers().antMatchers("/**")
                 .and()
                 .authorizeRequests()
+                .antMatchers(anonUrls).permitAll()
                 .antMatchers("/**").authenticated();
     }
+
 }
--- febs-server/febs-server-system/src/main/java/com/javaboy/febs/server/system/configure/FebsWebConfigure.java
+++ febs-server/febs-server-system/src/main/java/com/javaboy/febs/server/system/configure/FebsWebConfigure.java
@@ -3,17 +3,33 @@ package com.javaboy.febs.server.system.configure;
 import com.baomidou.mybatisplus.core.parser.ISqlParser;
 import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.javaboy.febs.server.system.properties.FebsServerSystemProperties;
+import com.javaboy.febs.server.system.properties.FebsSwaggerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /*
  * MyBatis Plus分页插件配置
  * */
 @Configuration
+// @EnableSwagger2标注，表示开启Swagger功能
+@EnableSwagger2
 public class FebsWebConfigure {
+
+    @Autowired
+    private FebsServerSystemProperties properties;
 
     @Bean
     public PaginationInterceptor paginationInterceptor() {
@@ -22,5 +38,37 @@ public class FebsWebConfigure {
         sqlParserList.add(new BlockAttackSqlParser());
         paginationInterceptor.setSqlParserList(sqlParserList);
         return paginationInterceptor;
+    }
+
+    /**
+     * swaggerApi方法的apis(RequestHandlerSelectors.basePackage("com.javaboy.febs.server.system.controller"))
+     * 表示将com.javaboy.febs.server.system.controller路径下的所有Controller都添加进去
+     *
+     * paths(PathSelectors.any())表示Controller里的所有方法都纳入
+     * @return Docket
+     */
+    @Bean
+    public Docket swaggerApi() {
+        FebsSwaggerProperties swagger = properties.getSwagger();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(swagger.getBasePackage()))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo(swagger));
+    }
+
+    /**
+     * apiInfo用于定义一些API页面信息，比如作者名称，邮箱，网站链接，开源协议等等
+     * @return ApiInfo
+     */
+    private ApiInfo apiInfo(FebsSwaggerProperties swagger) {
+        return new ApiInfo(
+                swagger.getTitle(),
+                swagger.getDescription(),
+                swagger.getVersion(),
+                null,
+                new Contact(swagger.getAuthor(), swagger.getUrl(), swagger.getEmail()),
+                swagger.getLicense(), swagger.getLicenseUrl(), Collections.emptyList());
     }
 }
